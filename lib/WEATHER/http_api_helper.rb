391b97622def6cc940439dda7d5624690108d823
John Edwards <jedwards@smartiqa.com>
2018-10-19 17:33:08 +0300

    SMART-110: Retriable gem added

--- lib/WEATHER/http_api_helper.rb
+++ lib/WEATHER/http_api_helper.rb
@@ -3,7 +3,7 @@ class HttpApiHelper < Http
   BASIC_URI            = '/data/2.5'
 
   REQUEST_RETRY_COUNT = 4
-  REQUEST_RETRY_INTERVAL = 15
+  REQUEST_RETRY_INTERVAL = 30
 
   def initialize(host, protocol, key)
     @host = host
@@ -13,7 +13,9 @@ class HttpApiHelper < Http
   end
 
   def info(city)
-    send_request('GET', "#{BASIC_URI}/weather", q: city, APPID: @key)
+    Retriable.retriable tries: REQUEST_RETRY_COUNT, base_interval: REQUEST_RETRY_INTERVAL do
+      send_request('GET', "#{BASIC_URI}/weather", q: city, APPID: @key)
+    end
   end
 
 end
