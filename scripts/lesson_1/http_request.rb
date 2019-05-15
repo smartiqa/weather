2f4e23ad2cf5e2cc79b1707325c44bfccd8578a9
Smartiqa Group <smartiqa@gmail.com>
2019-05-15 23:09:23 +0300

    GitHub API helper added

---
+++ scripts/lesson_1/http_request.rb
@@ -0,0 +1,68 @@
+require 'net/http'
+require 'json'
+
+module Lesson1
+
+  class Http
+
+    attr_reader :base_url
+
+    def initialize(host, protocol, headers = {}, auth_cred = nil)
+      @base_url ="#{protocol}://#{host}"
+      @headers = headers
+      @auth_cred = auth_cred
+    end
+
+    def send_request(method, url, params = nil, data = nil)
+      uri = URI.parse("#{@base_url}#{url}")
+      Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        case method
+          when 'GET'
+            uri.query = URI.encode_www_form(params) if params
+            request = Net::HTTP::Get.new(uri, @headers)
+          when 'POST'
+            request = Net::HTTP::Post.new(uri, @headers)
+            request.body = data.to_json
+          when 'PUT'
+            request = Net::HTTP::Put.new(uri, @headers)
+            request.body = data.to_json
+          when 'PATCH'
+            request = Net::HTTP::Patch.new(uri, @headers)
+            request.body = data.to_json
+          when 'DELETE'
+            request = Net::HTTP::Delete.new(uri, @headers)
+          else
+            raise "Request type is not defined! Provided type: #{method}"
+        end
+        request.basic_auth(@auth_cred[:username], @auth_cred[:password]) if @auth_cred
+        res = http.request(request)
+        raise "Http request failed. Response code is #{res.code}. Error message is #{res.body || 'empty'}" unless res.class == Net::HTTPOK
+        Http::handle_result(res.body)
+      end
+    end
+
+    private
+
+    def self.handle_result(res)
+      valid_json?(res) ? JSON.parse(res) : res
+    end
+
+    def self.valid_json?(json)
+      JSON.parse(json)
+      return true
+    rescue JSON::ParserError
+      return false
+    end
+
+  end
+
+end
+
+http = Lesson1::Http.new('httpbin.org', 'https')
+puts "Sending HTTP requests to #{http.base_url}..."
+http.send_request('GET', '/get')
+http.send_request('POST', '/post', nil, {test_key: 'test_value'})
+http.send_request('PUT', '/put', nil, {test_key: 'test_value'})
+http.send_request('PATCH', '/patch', nil, {test_key: 'test_value'})
+http.send_request('DELETE', '/delete')
+puts "HTTP requests to #{http.base_url} are successful!"
---
+++ scripts/lesson_2/github.rb
@@ -0,0 +1,145 @@
+require './http_request.rb'
+
+TEST_USER = 'smartiqa-test'
+# GitHub restricts pushing Authentication Token to public repo
+# That's why it's necessary to update it with valid token before running
+# AUTH_TOKEN = '<Please change me for b4a1d07f32....c5ca8594f>'
+TEST_REPO = 'test_repository_2'
+TEST_ISSUE_NUM = 3
+TEST_COMMIT_SHA = '063b6dde79957b9f34a0a5f74f4febe0e34cbba5'
+
+module Lesson2
+
+  class API
+
+    HOST = 'api.github.com'
+    PROTOCOL = 'https'
+
+    def initialize
+      @http = Http.new(HOST, PROTOCOL)
+    end
+
+    def call(method, relative_url, headers=nil, params=nil, json=nil)
+      @http.send_request(method, relative_url, headers, params, json)
+    end
+
+  end
+
+  class User
+
+    def initialize(auth_token=nil)
+      @api = API.new
+      # Majority of the API requests need Authentication Token but not all
+      @headers = {Authorization: "token #{auth_token}"} if auth_token
+    end
+
+
+    # ------ Get User info without Authentication ------
+    def get(user_name)
+      @api.call('GET', "/users/#{user_name}")
+    end
+
+    def get_bio( user_name)
+      get(user_name)['bio']
+    end
+
+    # -- Get additional User info with Authentication --
+    def get_authenticated
+      @api.call('GET', '/user', @headers)
+    end
+
+    # --------------- Update User info -----------------
+    def update_bio(new_bio)
+      @api.call('PATCH', '/user', @headers, nil, {bio: new_bio})
+    end
+
+  end
+
+  class Issue
+
+    def initialize(auth_token)
+      @api = API.new
+      @headers = {Authorization: "token #{auth_token}"}
+    end
+
+    # ------- List Issues -------------
+    def list
+      @api.call('GET', '/issues', @headers)
+    end
+
+    def list_for_repository(owner, repo)
+      @api.call('GET', "/repos/#{owner}/#{repo}/issues", @headers)
+    end
+
+    # ------- Get Issue info ----------
+    def get(owner, repo, number)
+      @api.call('GET', "/repos/#{owner}/#{repo}/issues/#{number}", @headers)
+    end
+
+    # ------- Edit Issues -------------
+    def edit_title(owner, repo, number, new_title)
+      edit(owner, repo, number, {title: new_title})
+    end
+
+    def edit_body(owner, repo, number, new_body)
+      edit(owner, repo, number, {body: new_body})
+    end
+    # ---------------------------------
+
+    private
+
+    def edit(owner, repo, number, json)
+      @api.call('PATCH', "/repos/#{owner}/#{repo}/issues/#{number}", @headers, nil, json)
+    end
+
+  end
+
+  class Repository
+
+    def initialize
+      @api = API.new
+    end
+
+    # ------- Work with Branches -------------
+    def list_branches(owner, repo)
+      @api.call('GET', "/repos/#{owner}/#{repo}/branches")
+    end
+
+    # ------- Work with Commits --------------
+    def list_commits(owner, repo, branch='master')
+      @api.call('GET', "/repos/#{owner}/#{repo}/commits", json={sha: branch})
+    end
+
+    def get_commit(owner, repo, commit_sha)
+      @api.call('GET', "/repos/#{owner}/#{repo}/commits/#{commit_sha}")
+    end
+
+    def get_commit_author(owner, repo, commit_sha)
+      get_commit(owner, repo, commit_sha)['author']
+    end
+
+  end
+
+end
+
+# Check GitHub User API functionality
+user = Lesson2::User.new(AUTH_TOKEN)
+user.get(TEST_USER)
+user.get_bio(TEST_USER)
+user.get_authenticated
+user.update_bio('New Test bio')
+
+# Check GitHub Issue API functionality
+issue = Lesson2::Issue.new(AUTH_TOKEN)
+issue.list
+issue.list_for_repository(TEST_USER, TEST_REPO)
+issue.get(TEST_USER, TEST_REPO, TEST_ISSUE_NUM)
+issue.edit_title(TEST_USER, TEST_REPO, TEST_ISSUE_NUM, 'Smartiqa Test issue 3')
+issue.edit_body(TEST_USER, TEST_REPO, TEST_ISSUE_NUM, 'Smartiqa Test body 3')
+
+# Check GitHub Repository API functionality
+repo = Lesson2::Repository.new
+repo.list_branches(TEST_USER, TEST_REPO)
+repo.list_commits(TEST_USER, TEST_REPO)
+repo.get_commit(TEST_USER, TEST_REPO, TEST_COMMIT_SHA)
+repo.get_commit_author(TEST_USER, TEST_REPO, TEST_COMMIT_SHA)
